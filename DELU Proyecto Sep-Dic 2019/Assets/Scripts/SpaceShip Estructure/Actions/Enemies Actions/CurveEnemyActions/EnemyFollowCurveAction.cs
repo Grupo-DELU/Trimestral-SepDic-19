a1c287ac79376d6dae1003f8d37fc705a5f27cda
xdgorgola <15-11264@usb.ve>
2019-12-14 23:32:01 -0430

    Naive path following

--- DELU Proyecto Sep-Dic 2019/Assets/Scripts/SpaceShip Estructure/Actions/Enemies Actions/CurveEnemyActions/EnemyFollowCurveAction.cs
+++ DELU Proyecto Sep-Dic 2019/Assets/Scripts/SpaceShip Estructure/Actions/Enemies Actions/CurveEnemyActions/EnemyFollowCurveAction.cs
@@ -6,11 +6,32 @@ public class EnemyFollowCurveAction : Action
 {
     public override void doAction(ActionManager manager)
     {
-        manager = manager as CurveEnemyManager;
-        //Deberiamos hacer un movement manager aparte? Que tenga datos como speed movement y eso
-        //Me parece que todo en una accion es muy aparatoso
-        //Igual que un shoot manager que contenga datos como firerate, tipo de proyectil y eso.
-        //Quizas el manager podria tener simplemente una referencia a estos managers?
+        CurveEnemyManager cm = manager as CurveEnemyManager;
+        cm.MoveWithVel(GetVelocity(cm));
+        if (GetVelocity(cm) != Vector2.zero) Debug.Log("Velocidad: " + GetVelocity(manager));
         Debug.Log("Enemigo moviendose");
     }
+
+    private Vector2 GetDirection(ActionManager manager)
+    {
+        CurveEnemyManager cm = manager as CurveEnemyManager;
+        Vector2 targ = (cm.Target - (Vector2)cm.transform.position);
+        //chequear esto en realidad
+        float dist = targ.magnitude;
+        //treshold modifical
+        if (dist < 0.5f)
+        {
+            cm.NextPoint();
+            return targ;
+        }
+        //Debug.Log("zorra maldita");
+        return (cm.Target - (Vector2)cm.transform.position).normalized;
+    }
+
+    private Vector2 GetVelocity(ActionManager manager)
+    {
+        CurveEnemyManager cm = manager as CurveEnemyManager;
+        return GetDirection(cm) * cm.fSpeed * cm.fMaxSpeed;
+    }
+
 }
--- DELU Proyecto Sep-Dic 2019/Assets/Scripts/SpaceShip Estructure/ActionsManagers/Enemies/CurveEnemyManager.cs
+++ DELU Proyecto Sep-Dic 2019/Assets/Scripts/SpaceShip Estructure/ActionsManagers/Enemies/CurveEnemyManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class CurveEnemyManager : ActionManager
 {
     /// <summary>
@@ -33,6 +34,18 @@ public class CurveEnemyManager : ActionManager
     /// </summary>
     private int iTargetIndex = 0;
 
+    public float fMaxSpeed = 10;
+    [Range(-1f, 1f)]
+    public float fSpeed = 1;
+
+    public Vector2 velocity = Vector2.zero;
+    private Rigidbody2D rb2d;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();    
+    }
+
     protected override void Start()
     {
         base.Start();
@@ -48,9 +61,23 @@ public class CurveEnemyManager : ActionManager
     /// <summary>
     /// Setea/calcula el proximo punto target del ennemigo
     /// </summary>
-    private void NextPoint()
+    public void NextPoint()
     {
         iTargetIndex = (iTargetIndex + 1) % path.points.Length;
         vTargetPos = path.points[iTargetIndex];
+    }
+
+    public void MoveWithVel(Vector2 velocity)
+    {
+        this.velocity = velocity;
+    }
+
+    private void FixedUpdate()
+    {
+        if (velocity != Vector2.zero)
+        {
+            rb2d.MovePosition((Vector2)transform.position + velocity * Time.fixedDeltaTime);
+            velocity = Vector2.zero;
+        }
     }
 }
