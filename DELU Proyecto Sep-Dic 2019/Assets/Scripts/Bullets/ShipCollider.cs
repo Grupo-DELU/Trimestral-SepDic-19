5a305b20b1c514d9bb78fd3cfe24ae0cab663970
Christian Oliveros <christianol_01@hotmail.com>
2020-01-03 23:06:12 -0400

    Ship Collider Test

---
+++ DELU Proyecto Sep-Dic 2019/Assets/Scripts/Bullets/ShipCollider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class ShipCollider : MonoBehaviour {
+
+    /// <summary>
+    /// Entity Manager
+    /// </summary>
+    private EntityManager _manager = null;
+
+    /// <summary>
+    /// Entity Associated with ship
+    /// </summary>
+    private Entity _entity;
+
+    /// <summary>
+    /// Register this Ship Collider
+    /// </summary>
+    /// <param name="entity">Associated Entity</param>
+    /// <param name="manager">Entity manager</param>
+    public void Register (Entity entity, EntityManager manager) {
+        _manager = manager;
+        _entity = entity;
+    }
+
+    private void Update () {
+        if (_manager != null) {
+            _manager.SetComponentData (_entity, new Translation { Value = transform.position });
+            _manager.SetComponentData (_entity, new Rotation { Value = transform.rotation });
+        }
+    }
+
+    private void OnDestroy () {
+        if (_manager != null && _manager.Exists (_entity)) {
+            _manager.DestroyEntity (_entity);
+        }
+    }
+}
---
+++ DELU Proyecto Sep-Dic 2019/Assets/Scripts/Bullets/ShipColliderConnector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class ShipColliderConnector : MonoBehaviour, IConvertGameObjectToEntity {
+    void IConvertGameObjectToEntity.Convert (Entity entity, EntityManager entityManager, GameObjectConversionSystem gameObjectConversionSystem) {
+        if (transform.parent == null) {
+            Debug.LogError ("Parent is not assigned", this);
+        }
+
+        ShipCollider shipCollider = transform.parent.GetComponent<ShipCollider> ();
+
+        if (shipCollider == null) {
+            Debug.LogError ("Parent ShipCollider is not present", this);
+        }
+        
+        shipCollider.Register (entity, entityManager);
+    }
+}
