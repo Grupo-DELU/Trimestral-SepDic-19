37c1a75d42670070a36ca1549ab99fb18cdd4c4d
Carlos Sivira <carlos.csivira@gmail.com>
2020-03-21 16:38:18 -0400

    Lazer Enemy Initialized

---
+++ DELU Proyecto Sep-Dic 2019/Assets/Scripts/LaserEnemy/LaserEnemyManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserEnemyManager : MonoBehaviour
+{
+    /// <summary>
+    /// Numero maximo de enemigos laser que posdrian salir a la vez
+    /// </summary>
+    public int max_enemies_in_screeen = 1;
+
+    /// <summary>
+    /// Prefab del enemigo laser a utilizar
+    /// </summary>
+    public GameObject enemy_laser_prefab;
+
+    /// <summary>
+    /// Ancho de la pantalla
+    /// </summary>
+    private int rect_width;
+
+    /// <summary>
+    /// Alto de la pantalla
+    /// </summary>
+    private int rect_height;
+
+    private GameObject[] laser_enemies;
+
+    private void Awake() {
+        Camera camera = GameObject.Find("SceneCamera").GetComponent<Camera>();
+        if (camera != null) {
+            rect_height = camera.pixelHeight;
+            rect_width = camera.pixelWidth;
+        }
+
+        if (rect_width < 1 || rect_height < 1)
+            Debug.LogError("Tamaño de camara invalido");
+        else if (max_enemies_in_screeen < 1)
+            Debug.LogError("No hay enemigos laser asignados");
+        else 
+            Debug.Log(Mathf.RoundToInt(rect_width / max_enemies_in_screeen));
+    }
+
+    private void Start(){
+        
+    }
+}
