a14b111c83f75ceafa61eab9bef9664b84658e8f
Carlos Sivira <carlos.csivira@gmail.com>
2020-03-22 20:49:01 -0400

    EnemyLaser
    Spawn, manager done

--- DELU Proyecto Sep-Dic 2019/Assets/Scripts/LaserEnemy/LaserEnemy.cs
+++ DELU Proyecto Sep-Dic 2019/Assets/Scripts/LaserEnemy/LaserEnemy.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class LaserEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    /// <summary>
+    /// Posicion inicial del enemigo laser
+    /// </summary>
+    private Vector3 initial_position;
+
+     /// <summary>
+    /// Numero que divide la pantalla verticalmente
+    /// </summary>
+    public int screen_height_divider;
+
+    /// <summary>
+    /// Numero que estable el tamaño del area de ataque
+    /// </summary>
+    public int attack_area_range;
+
+    /// <summary>
+    /// Punto fijado para el ataque
+    /// </summary>
+    private Vector3 target;
+
+    /// <summary>
+    /// Estado incial luego de su creacion
+    /// </summary>
+    /// <param name="pos">Posicion inicial del enemigo laser</param>
+    /// <param name="line">Altura del area de ataque</param>
+    /// <param name="area">Rango del area de ataque </param>
+    public void setSpawnPoint(Vector3 pos, int line, int area){
+        // Inicializacion de variables
+        screen_height_divider = line;
+        attack_area_range = area;
+        initial_position = pos;
+
+        // Estableciendo estado inicial del enemigo laser
+        transform.position = initial_position;
+        gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    /// <summary>
+    /// Busca y se ubica en la posicion desde donde iniciara el ataque
+    /// </summary>
+    public void moveToAttackPoint(){
+        int range = screen_height_divider + attack_area_range / 2;
+        Vector3 attack_point = new Vector3(0f, Random.Range(-range, range), 0f);
+
+        target = Camera.main.ScreenToWorldPoint(attack_point);
+        target += Vector3.right * transform.position.x;
+        transform.position = Vector3.MoveTowards(transform.position, target, 0.1f);
     }
+
+    /// <summary>
+    /// Dibuja gizmos en el editor
+    /// </summary>
+    void OnDrawGizmos(){
+        Gizmos.color = Color.white;
+
+        Gizmos.DrawLine(transform.position, target);
+        Gizmos.DrawSphere(target, 0.1f);
+    }
+
 }
--- DELU Proyecto Sep-Dic 2019/Assets/Scripts/LaserEnemy/LaserEnemyManager.cs
+++ DELU Proyecto Sep-Dic 2019/Assets/Scripts/LaserEnemy/LaserEnemyManager.cs
@@ -10,9 +10,24 @@ public class LaserEnemyManager : MonoBehaviour
     public int max_enemies_in_screeen = 1;
 
     /// <summary>
+    /// Numero de spawners
+    /// </summary>
+    public int number_of_spawners = 1;
+
+    /// <summary>
+    /// Numero que divide la pantalla verticalmente
+    /// </summary>
+    public int screen_height_divider = 3;
+
+    /// <summary>
+    /// Desplazamiento del area de ataque hacai arriba
+    /// </summary>
+    public int pixel_height_offset = 200;
+
+    /// <summary>
     /// Prefab del enemigo laser a utilizar
     /// </summary>
-    public GameObject enemy_laser_prefab;
+    public GameObject laser_enemy_prefab;
 
     /// <summary>
     /// Ancho de la pantalla
@@ -24,24 +39,93 @@ public class LaserEnemyManager : MonoBehaviour
     /// </summary>
     private int rect_height;
 
+    /// <summary>
+    /// Arreglo de enemigos laser
+    /// </summary>
     private GameObject[] laser_enemies;
 
     private void Awake() {
-        Camera camera = GameObject.Find("SceneCamera").GetComponent<Camera>();
+        Camera camera = Camera.main;
         if (camera != null) {
             rect_height = camera.pixelHeight;
             rect_width = camera.pixelWidth;
         }
 
         if (rect_width < 1 || rect_height < 1)
-            Debug.LogError("Tamaño de camara invalido");
-        else if (max_enemies_in_screeen < 1)
+            Debug.LogError("Tamaño de camara invalido" + rect_height + "--" + rect_width);
+        if (max_enemies_in_screeen < 1)
             Debug.LogError("No hay enemigos laser asignados");
-        else 
-            Debug.Log(Mathf.RoundToInt(rect_width / max_enemies_in_screeen));
     }
 
     private void Start(){
-        
+        setSpawnPoints();
+    }
+
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.Return)){
+            for (int i = 0; i < number_of_spawners; i++){
+                LaserEnemy script = laser_enemies[i].GetComponent<LaserEnemy>();
+                script.moveToAttackPoint();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Establece los puntos de aparicion de los enemigos laser basado en las
+    /// dimensiones de la pantalla y la cantidad de enemigos disponibles.
+    /// </summary>
+    void setSpawnPoints(){
+        laser_enemies = new GameObject[number_of_spawners];
+
+        int segment = Mathf.RoundToInt(rect_width / number_of_spawners);
+        int mid_point = Mathf.RoundToInt(segment / 2);
+
+        for (int i = 0; i < number_of_spawners; i++){
+            laser_enemies[i] = (GameObject)(Instantiate(laser_enemy_prefab));
+            LaserEnemy script = laser_enemies[i].GetComponent<LaserEnemy>();
+
+            Vector3 pos = new Vector3(mid_point + segment * i, rect_height, 0f);
+            Vector3 new_pos = Camera.main.ScreenToWorldPoint(pos);
+
+            // Inicializacion de la instancia del enemigo laser
+            script.setSpawnPoint(
+                new_pos, 
+                rect_height / 2 + pixel_height_offset,
+                rect_height / screen_height_divider + pixel_height_offset
+            );
+        }
+    }
+
+    /// <summary>
+    /// Dibuja gizmos en el editor
+    /// </summary>
+    void OnDrawGizmos() {
+        Vector2 from = new Vector2(0f, rect_height / 2 + pixel_height_offset);
+        Vector2 to = 
+            new Vector2(
+                rect_width, 
+                rect_height / 2 + pixel_height_offset
+            );
+
+        float range = rect_height / screen_height_divider + pixel_height_offset;
+
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(
+            Camera.main.ScreenToWorldPoint(from), 
+            Camera.main.ScreenToWorldPoint(to)
+        );
+
+        Gizmos.color = Color.blue;
+
+        Gizmos.DrawLine(
+            Camera.main.ScreenToWorldPoint(from + Vector2.up * range / 2), 
+            Camera.main.ScreenToWorldPoint(to + Vector2.up * range / 2)
+        );
+
+        Gizmos.DrawLine(
+            Camera.main.ScreenToWorldPoint(from + Vector2.down * range / 2), 
+            Camera.main.ScreenToWorldPoint(to + Vector2.down * range / 2)
+        );
     }
 }
