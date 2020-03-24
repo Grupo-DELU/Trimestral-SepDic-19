72f902187c31f56e69bd6cacb99f00222c0fe85f
xdgorgola <15-11264@usb.ve>
2020-03-24 03:09:39 -0430

    3D Model added

---
+++ DELU Proyecto Sep-Dic 2019/Assets/3DModels/GizmosGrid.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GizmosGrid : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                Gizmos.DrawWireCube(Vector3.right * j + Vector3.up * i, Vector3.one);
+                Gizmos.DrawWireCube(Vector3.right * -j + Vector3.up * -i, Vector3.one);
+                Gizmos.DrawWireCube(Vector3.right * j + Vector3.up * -i, Vector3.one);
+                Gizmos.DrawWireCube(Vector3.right * -j + Vector3.up * i, Vector3.one);
+            }
+        }
+    }
+}
+
+
